9df04a7b631f2ab6851d10cd66f5e1defb65f10f
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-07 13:22:33 -0400

    Track Operations

--- Classes/Queue.ts
+++ Classes/Queue.ts
@@ -1,22 +1,40 @@
 import { Snowflake, VoiceState } from "discord.js";
 import { AudioPlayer, AudioResource, createAudioPlayer } from '@discordjs/voice';
 import { EventEmitter } from 'events';
+import Track from "./Track";
 
 export default class Queue {
 
     guildId: Snowflake
     channelId: Snowflake
     player: AudioPlayer
-    resources: AudioResource[]
+
+    tracks: Track[]
+    currentTrackId: number
+    currentResource: AudioResource
+
     events: EventEmitter
     eventNames: { track: { load: "trackLoad", end: "trackEnd" }, voice: { join: "joinChannel", leave: "leaveChannel" } }
+
+    settings : {
+        
+        loop: {
+            trackLoop: boolean,
+            queueLoop: boolean,
+        },
+
+        shuffle: boolean
+
+    }
 
     constructor(guildId: Snowflake, channelId: Snowflake) {
 
         this.guildId = guildId
         this.channelId = channelId
         this.player = createAudioPlayer()
-        this.resources = []
+
+        this.tracks = []
+
         this.events = new EventEmitter()
         this.eventNames = { 
             track: { 
@@ -29,9 +47,80 @@ export default class Queue {
             } 
         }
 
+        // Run the listeners
+        this.listeners()
+
     }
 
-    listen() {
+    /*
+    
+    - Move Track
+    - Go To Track Index
+
+    - Visualize Queue
+
+    (Later)
+    - Toggle Shuffle
+    - Loop
+    - Seek
+
+    */
+
+    add(track: Track) {
+
+        // Add the new track to the array of tracks
+        this.tracks.push(track)
+
+    }
+
+
+    async remove(trackId: number) {
+
+        // Find the track
+        const trackIndex = this.tracks.findIndex(track => track.Id == trackId)
+
+        if (trackIndex != -1) {
+
+            if (trackIndex == this.currentTrackId) {
+
+                // The track to be removed is the current track
+
+                // Go to the next track
+                await this.goto(trackIndex + 1).catch((err) => { throw err; })
+
+            }
+
+            // Delete the track
+            this.tracks.splice(trackIndex, 1)
+
+            return true;
+
+        } else {
+
+            // Track was not found
+
+            return false;
+
+        }
+
+    }
+
+
+    async goto(trackIndex: number) {
+
+        // Find the requested track
+        const track = this.tracks[trackIndex]
+            if (!track) return false;
+
+        // Load the track
+        const resource = await track.load().catch((err) => { throw err; })
+            this.currentResource = resource
+        
+
+
+    }
+
+    listeners() {
 
         this.events.on(this.eventNames.voice.join, (oldState: VoiceState, newState: VoiceState) => {
 
--- Classes/Track.ts
+++ Classes/Track.ts
@@ -1,5 +1,58 @@
+import { AudioResource, createAudioResource } from "@discordjs/voice"
+
+export async function createResource(track: Track, seekSec?: number) {
+
+    if (track.sourceType == "YOUTUBE") {
+
+        let stream = await playdl.stream(track.source, {
+            seek: seekSec ? seekSec : 0,
+        }).catch(err => {
+            throw err;
+        })
+        return createAudioResource(stream.stream, {
+            inputType: stream.type,
+        });
+
+    } else if (track.sourceType == "DISCORD") {
+
+        return createAudioResource(track.source);
+
+    }
+
+}
+
 export default class Track {
 
+    // Give each track an ID
+    // The ID can just be a number
+    // To prevent overlaps, just increment a number from the greatest number already existing in the queue; this will refer to this track.
+
+    // Basic Track Info \\
+    Id: number // Internal Id assigned on creation
+    title: string
+    author: string
+    durationSec: number
+    sourceType: "YOUTUBE" | "DISCORD"
+    source: string // URL
+    shufflePlayed: boolean // Was the track already played in the shuffle queue or not?
+
+    private resource: AudioResource<unknown> | undefined
+
+    constructor(title: string, author: string, durationSec: number, sourceType: "DISCORD" | "YOUTUBE", source: string, shufflePlayed?: boolean) {
+        this.title = title
+        this.author = author
+        this.durationSec = durationSec
+        this.sourceType = sourceType
+        this.source = source
+        this.shufflePlayed = shufflePlayed ? shufflePlayed : false
+    }
+
+    async load() {
+        
+        this.resource = await createResource(this).catch((err) => { throw err; }) as AudioResource
+        return this.resource
+
+    }
     
 
 }
